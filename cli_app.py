81716bf4dec855dea32e8ab247ee3f0bb770c7b4
YunanLiu06 <112599210+YunanLiu06@users.noreply.github.com>
2022-09-27 03:29:08 +0000

    add cli_app

---
+++ cli_app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import click
+from querydb import querydb
+from querydb import sortbyyearDESC
+
+
+@click.group()
+def cli():
+    """click group cli subcommand test"""
+
+@cli.command()
+@click.option('--query', help = "Query the first two lines of file in databricks")
+def queryFunc():
+    """Query function!"""
+
+    result = querydb()
+    click.echo(result)
+
+@cli.command()
+@click.option('--sort', help = "Sort data in the databricks by year descending")
+def queryFunc():
+    """Sort data!"""
+
+    result = sortbyyearDESC()
+    click.echo(result)
+
+if __name__ == "__main__":
+    cli()
